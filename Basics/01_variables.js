3e8561664fd1812b40e0ea4f7ec8bff96403aeb4
Subhrajeet1995 <subhrajeetbehera2@gmail.com>
2025-02-08 16:50:50 +0000

    LetVarConst

---
+++ Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 124563
+let accountEmail = "subh@google.com"
+var accountPassword = "14896"
+accountCity = "Keonjhar"
+let accountState = ""
+
+//accountId = 2
+
+accountEmail = "srry@yu.com"
+accountPassword = "656192"
+accountCity = "Andapur"
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// Do not use Var because of issue in block scope and functional scope
