28dfaba051d59aa6521fb95a5dc7dedb656fa3e7
Subhrajeet1995 <subhrajeetbehera2@gmail.com>
2025-03-15 09:34:02 +0000

    Datatype summery

--- Basics/conversationOperstion.js
+++ Basics/conversationOperstion.js
@@ -1,4 +1,25 @@
-let score = "33abc"
-console.log(typeof score);
-let valueInNumber = Number(score)
-console.log(valueInNumber);
+//let score = "33abc"
+//console.log(typeof score);
+//let valueInNumber = Number(score)
+//console.log(valueInNumber);
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log(id == anotherId);
+//console.log(id);
+//console.log(typeof anotherId);
+let userone = "Subhrajeet"
+console.log(userone);
+let usertwo = "userone"
+console.log(userone);
+console.log(usertwo);
+
+
+let user1 = 
+{
+    email:"subhrajeetbehera2@gmail.com",
+    UID:"1234567"
+}
+
+console.log(user1);
+let user2 = user1
+console.log(user2);
